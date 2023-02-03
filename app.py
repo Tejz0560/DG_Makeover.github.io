c0276b0eb2b8b1a9f11db9eecd17e3bd182550a3
Tejz0560 <59393684+Tejz0560@users.noreply.github.com>
2023-02-03 20:34:00 +0530

    initial

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/book")
+def book():
+    return render_template("book.html")
+
+@app.route("/offers")
+def offers():
+    return render_template("offers.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
